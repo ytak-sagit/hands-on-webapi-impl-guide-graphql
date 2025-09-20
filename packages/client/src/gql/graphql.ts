118d41dbe87651d450b12867e4d239baed2d2637
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-09-20 22:05:16 +0900

    feat: GraphQLスキーマから型定義を生成

---
+++ packages/client/src/gql/graphql.ts
@@ -0,0 +1,63 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = T | null | undefined;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Author = User & {
+  __typename?: 'Author';
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
+  posts: Array<Post>;
+};
+
+export type Post = {
+  __typename?: 'Post';
+  author: Author;
+  body: Scalars['String']['output'];
+  id: Scalars['ID']['output'];
+  tags: Array<PostTag>;
+  title: Scalars['String']['output'];
+};
+
+export type PostTag = {
+  __typename?: 'PostTag';
+  name: Scalars['String']['output'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  authors: Array<Author>;
+  posts: Array<Post>;
+};
+
+
+export type QueryPostsArgs = {
+  tags?: InputMaybe<Array<Scalars['String']['input']>>;
+};
+
+export type User = {
+  name: Scalars['String']['output'];
+};
+
+export type PostsQueryVariables = Exact<{
+  tags?: InputMaybe<Array<Scalars['String']['input']> | Scalars['String']['input']>;
+}>;
+
+
+export type PostsQuery = { __typename?: 'Query', posts: Array<{ __typename?: 'Post', title: string, body: string, author: { __typename?: 'Author', name: string } }> };
+
+
+export const PostsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"posts"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"tags"}},"type":{"kind":"ListType","type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"String"}}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"posts"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"tags"},"value":{"kind":"Variable","name":{"kind":"Name","value":"tags"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"body"}},{"kind":"Field","name":{"kind":"Name","value":"author"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]} as unknown as DocumentNode<PostsQuery, PostsQueryVariables>;
