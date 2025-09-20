d05d702b0aeb49d97061ee81101c4f638739ea9d
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-09-20 20:57:55 +0900

    feat: WebFEからGraphQLクエリを発行できるように対応

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { gql, useQuery } from "urql";
+
+const PostsQuery = gql`
+  query posts($tags: [String!]) {
+    posts(tags: $tags) {
+      title
+      body
+      author {
+        name
+      }
+    }
+  }
+`;
+
+const ChoosableTags = [
+  "GraphQL",
+  "Prisma",
+  "Observability",
+  "Engineering",
+  "Event",
+  "ADR",
+  "CSS",
+  "執筆",
+  "example",
+] as const;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tags, setTags] = useState(["GraphQL"]);
+
+  const [result] = useQuery({
+    query: PostsQuery,
+    variables: { tags },
+  });
+
+  const { data, fetching, error } = result;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>GraphQL Client Sample</h1>
+      <fieldset>
+        <legend>Choose Blog tags:</legend>
+
+        {ChoosableTags.map((choosableTag) => (
+          <div key={choosableTag}>
+            <input
+              type="checkbox"
+              id={choosableTag}
+              name={choosableTag}
+              checked={tags.includes(choosableTag)}
+              onChange={(e) => {
+                e.target.checked
+                  ? setTags([...tags, e.target.name])
+                  : setTags(tags.filter((t) => t !== e.target.name));
+              }}
+            />
+            <label htmlFor={choosableTag}>{choosableTag}</label>
+          </div>
+        ))}
+      </fieldset>
+
+      {fetching ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Oh no... {error.message}</p>
+      ) : (
+        <ul>
+          {data?.posts.map((post) => (
+            <li key={post.title}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <p>Author: {post.author.name}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
