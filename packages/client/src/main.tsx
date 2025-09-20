91dde412544c48d4bce965aafb9cc76353d70bc1
ytak-sagit <omega.sagittarius.xxx+tech@gmail.com>
2025-09-20 20:56:25 +0900

    feat: urqlのClientを使用できるように対応

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Client, cacheExchange, fetchExchange, Provider } from "urql";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const client = new Client({
+  url: "http://localhost:4000",
+  exchanges: [cacheExchange, fetchExchange],
+  // NOTE: 以下のエラー回避のためにヘッダーをセットしている
+  // [GraphQL] This operation has been blocked as a potential Cross-Site Request Forgery (CSRF).
+  fetchOptions: () => {
+    return {
+      headers: {
+        "x-apollo-operation-name": "apollo-require-preflight",
+      },
+    };
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
